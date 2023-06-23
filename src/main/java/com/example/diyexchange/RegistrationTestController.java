ff3d1679bf8f27d98ee760be1ad00064f488d5c7
mpanczak <panczakmateusz@gmail.com>
2023-06-23 15:10:06 +0200

    spring security demo

---
+++ src/main/java/com/example/diyexchange/RegistrationTestController.java
@@ -0,0 +1,37 @@
+package com.example.diyexchange;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+@RequiredArgsConstructor
+public class RegistrationTestController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping
+    @ResponseBody
+    public boolean createTestUser(){
+
+        User user = new User();
+        user.setLogin("Tomasz2");
+
+        user.setPassword(passwordEncoder.encode("Tomasz"));
+
+        userRepository.save(user);
+
+
+        User user2 = new User();
+        user2.setLogin("Tomasz");
+
+        user2.setPassword(passwordEncoder.encode("Tomasz"));
+        userRepository.save(user2);
+        return true;
+    }
+}
---
+++ src/test/java/com/example/diyexchange/DiYexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiYexchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
