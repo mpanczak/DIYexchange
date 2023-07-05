fdc432d814516c94e4aa3f68018e8d6fd83e19db
mpanczak <panczakmateusz@gmail.com>
2023-07-05 15:28:59 +0200

    allow css and js

--- src/main/java/com/example/diyexchange/config/SecurityConfiguration.java
+++ src/main/java/com/example/diyexchange/config/SecurityConfiguration.java
@@ -20,8 +20,8 @@ public class SecurityConfiguration {
         );
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home", "/test", "/register", "/pictures/*").permitAll()
-//                        .requestMatchers("/**").permitAll()
+//                        .requestMatchers("/", "/home", "/test", "/register", "/pictures/*").permitAll()
+                        .requestMatchers("/**", "/**/*.{js,css}").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
