a30f84486b4408c2de621e8663c232bf46880ad7
mpanczak <panczakmateusz@gmail.com>
2023-07-05 19:11:31 +0200

    header style css

--- src/main/resources/static/js/actions.js
+++ src/main/resources/static/js/actions.js
@@ -1,3 +1,15 @@
 function showAlert() {
     alert("The button was clicked!");
 }
+
+function followUser() {
+    alert("The button was clicked!");
+}
+
+function likePost() {
+    alert("The button was clicked!");
+}
+
+function comment() {
+    alert("The button was clicked!");
+}
