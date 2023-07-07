c4a1c23e322d93208b4fa267bf5cb85c3a04e0ff
mpanczak <panczakmateusz@gmail.com>
2023-07-07 15:35:24 +0200

    async mail sending

--- src/main/java/com/example/diyexchange/service/PostService.java
+++ src/main/java/com/example/diyexchange/service/PostService.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @Transactional
@@ -79,6 +80,8 @@ public class PostService {
         post.setUser(userService.retrieveLoggedUser());
         postRepository.save(post);
 
-        notificationService.sendNotifications(post);
+        CompletableFuture.runAsync(() -> {
+            notificationService.sendNotifications(post);
+        });
     }
 }
